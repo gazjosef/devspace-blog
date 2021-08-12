20073319abca90a831c74989f3417e9b8c2efe04
Gareth Hind <garethjhind@gmail.com>
2021-08-12 15:53:31 +1000

    Get Posts & Parse Frontmatter

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -7,9 +7,3 @@ export default function BlogPage() {
     </Layout>
   );
 }
-
-export async function getStaticProps() {
-  return {
-    props: {},
-  };
-}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import fs from "fs";
+import path from "path";
 import Layout from "../components/Layout";
 
 export default function HomePage() {
@@ -6,4 +8,28 @@ export default function HomePage() {
       <h1>Hello World</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+
+    console.log(markdownWithMeta);
+    return {
+      slug,
+    };
+  });
+
+  console.log(posts);
+
+  return {
+    props: {},
+  };
 }
