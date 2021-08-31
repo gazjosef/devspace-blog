aeab28f02f98f1045bef60a640ebca8f41d111a6
Gareth Hind <garethjhind@gmail.com>
2021-08-31 12:36:53 +1000

    Category Sidebar

--- components/CategoryList.js
+++ components/CategoryList.js
@@ -6,6 +6,13 @@ export default function CategoryList({ categories }) {
       <h3 className="text-2xl bg-gray-800 text-white p-3 rounded">
         Blog Categories
       </h3>
+      <ul className="divide-y divide-gray-300">
+        {categories.map((category, index) => (
+          <Link key={index} href={`/blog/category/${category.toLowerCase()}`}>
+            <li className="p-4 cursor-pointer hover:bg-gray-50">{category}</li>
+          </Link>
+        ))}
+      </ul>
     </div>
   );
 }
